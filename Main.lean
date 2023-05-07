9bba78eb1d4a1597675c441af2e008a2cb0dbb7e
Leni Aniva <v@leni.sh>
2023-05-07 15:19:45 -0700

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Pantograph
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,19 @@
+import Lake
+open Lake DSL
+
+
+package pantograph {
+  -- add package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @  "8e5a00a8afc8913c0584cb85f37951995275fd87"
+
+lean_lib Pantograph {
+  -- add library configuration options here
+}
+
+@[default_target]
+lean_exe pantograph {
+  root := `Main
+}
